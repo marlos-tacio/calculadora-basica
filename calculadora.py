228826ba98982d135d4c14e8e74707ab60cebd5c
Marlos <marlos.tacio@gmail.com>
2025-04-09 16:33:34 -0300

    Adiciona potência usando operador **

--- calculadora.py
+++ calculadora.py
@@ -11,3 +11,6 @@ def divisao(a, b):
     if b == 0:
         raise ValueError("Divisão por zero!")
     return a / b
+
+def potencia(base, expoente):  
+    return base ** expoente  # Usando operador **
